d4172a9028fa0101bfa897981ba138a88fd9d9d0
JonathanAriass <UO283586@uniovi.es>
2024-08-30 22:04:09 +0200

    First try on gh-pages

--- src/utils/test.ts
+++
@@ -1,6 +0,0 @@
-// src/utils/test.ts
-import Card from "@components/Card.astro";
-
-export default function Test() {
-    return (<Card title="Hello, Astro!" body="This is a sample body text." href="https://example.com" />);
-}
