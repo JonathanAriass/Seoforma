9faa7e376e6d80d3e40a5c393cba779c96e1a913
JonathanAriass <UO283586@uniovi.es>
2024-09-03 22:24:32 +0200

    Change site on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://JonathanAriass.github.io',
-  base: '/Seoforma'
+  site: 'www.seoforma.es',
 });
