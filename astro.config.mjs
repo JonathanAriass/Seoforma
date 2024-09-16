ddf2d94f0bcc0ad95b3e25a56926daa50604a453
JonathanAriass <UO283586@uniovi.es>
2024-09-16 21:21:20 +0200

    Add PartyTown to fix Google Analytics error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    })
+  ],
   site: 'http://www.seoforma.es',
 });
